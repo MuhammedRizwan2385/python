29d404b2c229a7f64a82458ebf8f3725ef73e2f6
MuhammedRizwan2385 <mohammedrizwan2385@gmail.com>
2025-05-11 22:46:25 +0530

    Create turtle_T.py

---
+++ turtle_T.py
@@ -0,0 +1,13 @@
+#Drawing T with 2 colors
+import turtle
+t=turtle.Turtle()
+t.pencolor("blue")
+t.forward(100)
+t.up()
+t.left(180)
+t.pencolor("red")
+t.forward(50)
+t.down()
+t.left(90)
+t.forward(100)
+turtle.done()
